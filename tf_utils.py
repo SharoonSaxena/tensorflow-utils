c3667feb1b506f5e7403f485124dfe2be83db6b8
Sharoon Saxena <charismaticsharoon@gmail.com>
2021-06-08 08:03:14 +0530

    Create tf_utils.py

---
+++ tf_utils.py
@@ -0,0 +1,13 @@
+def try_gpu(i=0):  #@save
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if len(tf.config.experimental.list_physical_devices('GPU')) >= i + 1:
+        return tf.device(f'/GPU:{i}')
+    return tf.device('/CPU:0')
+
+def try_all_gpus():  #@save
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    num_gpus = len(tf.config.experimental.list_physical_devices('GPU'))
+    devices = [tf.device(f'/GPU:{i}') for i in range(num_gpus)]
+    return devices if devices else [tf.device('/CPU:0')]
+
+try_gpu(), try_gpu(10), try_all_gpus()
